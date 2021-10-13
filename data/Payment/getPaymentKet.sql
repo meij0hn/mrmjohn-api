1eac216504d3639aa1f91805c1f19cd0264d0677
Muhammad Meijohn <meijohnnura@gmail.com>
2021-10-13 11:54:11 +0700

    20211013_1154
    20211013_1154

--- data/Payment/getPaymentKet.sql
+++ data/Payment/getPaymentKet.sql
@@ -1,4 +1,5 @@
-select	distinct tph.transno, tpd.ket
+select	distinct tbh.bookcode, tph.transno, tpd.ket
 from    TR_Paymentheader tph
 join    TR_PaymentDetail tpd ON tph.id = tpd.paymentheaderid
+left join TR_bookingheader tbh ON tph.bookingheaderid = tbh.id
 where	transno = @transno
